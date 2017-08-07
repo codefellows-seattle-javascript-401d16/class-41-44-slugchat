4542632818d663e5b8cecb523c5f114ce5ee2e1c
Kyle Aardal <aardal.kyle@gmail.com>
2017-08-07 15:58:29 -0700

    succesfully recieves google token

--- slugchat-backend-starter-code/src/router/auth.js
+++ slugchat-backend-starter-code/src/router/auth.js
@@ -1,34 +1,37 @@
-'use strict'
+"use strict";
 
-import {Router} from 'express'
-import User from '../model/user.js'
-import bodyParser from 'body-parser'
-import basicAuth from '../middleware/basic-auth.js'
+import { Router } from "express";
+import User from "../model/user.js";
+import bodyParser from "body-parser";
+import basicAuth from "../middleware/basic-auth.js";
 
 export default new Router()
-.post('/signup', bodyParser.json() , (req, res, next) => {
-  new User.createFromSignup(req.body)
-  .then(user => user.tokenCreate())
-  .then(token => {
-    res.cookie('X-Slugchat-Token', token)
-    res.send(token)
+  .get("oauth/google/code", (req, res, next) => {
+    if (!req.query.code) res.redirect(process.env.CLIENT_URL);
   })
-  .catch(next)
-})
-.get('/usernames/:username', (req, res, next) => {
-  User.findOne({username: username})
-  .then(user => {
-    if(!user)
-      return res.sendStatus(409)
-    return res.sendStatus(200)
+  .post("/signup", bodyParser.json(), (req, res, next) => {
+    new User.createFromSignup(req.body)
+      .then(user => user.tokenCreate())
+      .then(token => {
+        res.cookie("X-Slugchat-Token", token);
+        res.send(token);
+      })
+      .catch(next);
   })
-  .catch(next)
-})
-.get('/login', basicAuth, (req, res, next) => {
-  req.user.tokenCreate()
-  .then((token) => {
-    res.cookie('X-Slugchat-Token', token)
-    res.send(token)
+  .get("/usernames/:username", (req, res, next) => {
+    User.findOne({ username: username })
+      .then(user => {
+        if (!user) return res.sendStatus(409);
+        return res.sendStatus(200);
+      })
+      .catch(next);
   })
-  .catch(next)
-})
+  .get("/login", basicAuth, (req, res, next) => {
+    req.user
+      .tokenCreate()
+      .then(token => {
+        res.cookie("X-Slugchat-Token", token);
+        res.send(token);
+      })
+      .catch(next);
+  });

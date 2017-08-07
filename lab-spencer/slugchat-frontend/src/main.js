548c24226536499e03132f98bd477c1471bbe7e2
Spencer Gietzen <Speng1516@comcast.net>
2017-08-07 15:57:19 -0700

    backend copied in, frontend set up and configged

---
+++ lab-spencer/slugchat-frontend/src/main.js
@@ -0,0 +1,20 @@
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Main extends React.Component {
+  constructor() {
+    const oAuthGoogleParams = {
+
+    };
+    this.state = oAuthGoogleParams;
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+ReactDom(<Main />, document.getElementById('root'));

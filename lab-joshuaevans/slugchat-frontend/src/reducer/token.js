bb3f31b559a2902bf72eea6cd8d7768dc9e590a5
Joshua Evans <skyfriends1@gmail.com>
2017-08-08 15:05:52 -0700

    fixed airbnb linter rules

--- lab-joshuaevans/slugchat-frontend/src/reducer/token.js
+++ lab-joshuaevans/slugchat-frontend/src/reducer/token.js
@@ -1,9 +1,10 @@
-export default (state=null, {type, payload}) => {
-  switch(type) {
-  case 'LOGIN':
-    return payload;
-  case 'LOGOUT':
-    return null;
-
+export default (state = null, { type, payload }) => {
+  switch (type) {
+    case 'LOGIN':
+      return payload;
+    case 'LOGOUT':
+      return null;
+    default:
+      return state;
   }
 };

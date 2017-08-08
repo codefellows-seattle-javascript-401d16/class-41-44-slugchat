f56f1cf9381b3b822e5559e04fb03570ad9812d4
Oscar Cauich <ocauich@gravitypayments.com>
2017-08-07 17:46:14 -0700

    oauth is working on redirecting correctly

--- lab-oscar/slugchat-backend/src/router/auth.js
+++ lab-oscar/slugchat-backend/src/router/auth.js
@@ -4,6 +4,7 @@ import {Router} from 'express'
 import User from '../model/user.js'
 import bodyParser from 'body-parser'
 import basicAuth from '../middleware/basic-auth.js'
+import superagent from 'superagent';
 
 export default new Router()
 .post('/signup', bodyParser.json() , (req, res, next) => {
@@ -31,4 +32,39 @@ export default new Router()
     res.send(token)
   })
   .catch(next)
+})
+
+.get('/oauth/google/code', (req, res, next) => {
+  if(!req.query.code){
+    //user has no accesss
+    res.redirect(process.env.CLIENT_URL)
+  } else {
+    //exchange the code for a google access token
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
+    .type('form')
+    .send({
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri:  `${process.env.API_URL}/oauth/google/code`,
+    })
+    .then(response => {
+      return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
+      .set('Authorization', `Bearer ${response.body.access_token}`)
+    })
+    .then(response => {
+
+      return User.handleOAUTH(response.body)
+    })
+    .then(user => user.tokenCreate())
+    .then(token => {
+      res.cookie('X-Gravity-Token', token)
+      res.redirect(process.env.CLIENT_URL)
+    })
+    .catch((error) => {
+      console.log('!!!!', error);
+      res.redirect(process.env.CLIENT_URL)
+    })
+  }
 })

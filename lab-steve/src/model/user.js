571aa8e29da1604f6d7db783c14cfc7fdd325297
0smium <steve.walsh@transfueler.com>
2017-08-08 16:06:54 -0700

    41 complete

--- lab-steve/src/model/user.js
+++ lab-steve/src/model/user.js
@@ -1,60 +1,94 @@
-'use strict'
+'use strict';
 
 // DEPENDECIES
-import * as bcrypt from 'bcrypt'
-import {randomBytes} from 'crypto'
-import * as jwt from 'jsonwebtoken'
-import createError from 'http-errors'
-import {promisify} from '../lib/promisify.js'
-import Mongoose, {Schema} from 'mongoose'
+import * as bcrypt from 'bcrypt';
+import {randomBytes} from 'crypto';
+import * as jwt from 'jsonwebtoken';
+import createError from 'http-errors';
+import {promisify} from '../lib/promisify.js';
+import Mongoose, {Schema} from 'mongoose';
 
 // SCHEMA
 const userSchema =  new Schema({
   email: {type: String, required: true, unique: true},
   username: {type: String, required: true, unique: true},
-  passwordHash: {type: String, required: true},
+  passwordHash: {type: String, required: false},
   tokenSeed: {type: String,  unique: true, default: ''},
-})
+});
 
 // INSTANCE METHODS
 userSchema.methods.passwordCompare = function(password){
   return bcrypt.compare(password, this.passwordHash)
-  .then(success => {
-    if (!success)
-      throw createError(401, 'AUTH ERROR: wrong password')
-    return this
-  })
-}
+    .then(success => {
+      if (!success)
+        throw createError(401, 'AUTH ERROR: wrong password');
+      return this;
+    });
+};
 
 userSchema.methods.tokenCreate  = function(){
-  this.tokenSeed = randomBytes(32).toString('base64')
+  this.tokenSeed = randomBytes(32).toString('base64');
   return this.save()
-  .then(user => {
-    return jwt.sign({tokenSeed: this.tokenSeed}, process.env.SECRET)
-  })
-  .then(token => {
-    return token
-  })
-}
+    .then(user => {
+      return jwt.sign({tokenSeed: this.tokenSeed}, process.env.SECRET);
+    })
+    .then(token => {
+      return token;
+    });
+};
 
 // MODEL
-const User = Mongoose.model('user', userSchema)
+const User = Mongoose.model('user', userSchema);
 
 // STATIC METHODS
+
+// for form signup
 User.createFromSignup = function (user) {
   if(!user.password || !user.email || !user.username)
     return Promise.reject(
-      createError(400, 'VALIDATION ERROR: missing username email or password '))
+      createError(400, 'VALIDATION ERROR: missing username email or password '));
 
-  let {password} = user
-  user = Object.assign({}, user, {password: undefined})
+  let {password} = user;
+  user = Object.assign({}, user, {password: undefined});
 
   return bcrypt.hash(password, 1)
-  .then(passwordHash => {
-    let data = Object.assign({}, user, {passwordHash}) 
-    return new User(data).save()
-  })
-}
+    .then(passwordHash => {
+      let data = Object.assign({}, user, {passwordHash});
+      return new User(data).save();
+    });
+};
+
+// for OAUTH signup
+User.handleOAUTH = function(profile) {
+  if(!profile)
+    return Promise.reject(
+      createError(400, 'VALIDATION ERROR: no profile received!')
+    );
+  if(!profile.email)
+    return Promise.reject(
+      createError(400, 'VALIDATION ERROR: no email address received with profile!')
+    );
+  return User.findOne({email: profile.email}) //check if user exists
+    .then(user => {
+      if(!user)
+        throw new Error('Existing user not found.');
+      console.log('Logging in existing user');
+      return user;
+    })
+    .catch(() => {
+      console.log('Creating new user with GPlus profile data.');
+      return new User({
+        username: `${profile.given_name}.${profile.family_name}`,
+        email: profile.email,
+      });
+    });
+};
+
+
+
+
+
+
 
 // INTERFACE
-export default User
+export default User;
--- lab-steve/src/router/auth.js
+++ lab-steve/src/router/auth.js
@@ -7,6 +7,44 @@ import basicAuth from '../middleware/basic-auth.js';
 import superagent from 'superagent';
 
 export default new Router()
+
+  .get('/oauth/google/code', (req, res, next) => {
+    console.log('req.query: ', req.query);
+    if(!req.query.code) {
+      res.redirect(process.env.CLIENT_URL);
+    } else {
+      superagent.post(process.env.OAUTH_API)
+        .type('form')
+        .send({
+          code: req.query.code,
+          grant_type: 'authorization_code',
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET,
+          redirect_uri: `${process.env.API_URL}/oauth/google/code`,
+        })
+        .then(res => {
+          console.log('Google token data: ', res.body);
+          return superagent.get(process.env.GPLUS_API)
+            .set('Authorization', `Bearer ${res.body.access_token}`);
+        })
+        .then(res => {
+          console.log('Google profile: ', res.body);
+          return User.handleOAUTH(res.body);
+        })
+        .then(user => user.tokenCreate())
+        .then(token => {
+          res.cookie('X-Slugchat-Token', token);
+          res.redirect(process.env.CLIENT_URL);
+        })
+        .catch((next) => {
+          console.log(next);
+          res.redirect(process.env.CLIENT_URL);
+        });
+    }
+  })
+
+
+
   .post('/signup', bodyParser.json() , (req, res, next) => {
     new User.createFromSignup(req.body)
       .then(user => user.tokenCreate())

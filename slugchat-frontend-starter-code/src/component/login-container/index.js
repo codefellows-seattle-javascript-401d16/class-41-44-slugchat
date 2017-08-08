05d01ffc20e7f36e9c9685c3ba134f6df9d9b17f
Matthew Parker <cadburylion@outlook.com>
2017-08-08 16:43:39 -0700

    Added functioning login form, no validation

--- slugchat-frontend-starter-code/src/component/login-container/index.js
+++ slugchat-frontend-starter-code/src/component/login-container/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import * as _ from 'lodash'
 import {connect} from 'react-redux'
 import superagent from 'superagent'
+import * as util from '../../lib/util.js'
+import * as auth from '../../action/auth.js'
 
 class LoginContainer extends React.Component {
   constructor(props){
@@ -10,12 +12,56 @@ class LoginContainer extends React.Component {
       username: '',
       password: '',
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleSubmit(e){
+    e.preventDefault()
+    return this.props.login({
+      username: this.state.username,
+      password: this.state.password,
+    })
+  }
+
+  handleChange(e){
+    let {name, value} = e.target
+    this.setState({[name]: value})
+  }
+
   render(){
     return(
-      <h2>Login Form</h2>
+      <div className='login-container'>
+      <form onSubmit={this.handleSubmit}>
+
+        <input
+          name='username'
+          type='text'
+          placeholder='username'
+          value={this.state.username}
+          onChange={this.handleChange}
+          />
+
+        <input
+          name='password'
+          type='password'
+          placeholder='password'
+          value={this.state.password}
+          onChange={this.handleChange}
+          />
+
+        <button type='submit'> login </button>
+      </form>
+      </div>
     )
   }
 }
 
-export default LoginContainer
+export const mapStateToProps = (state) => ({
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  login: (user) => dispatch(auth.loginRequest(user)),
+})
+
+export default connect (mapStateToProps, mapDispatchToProps)(LoginContainer)

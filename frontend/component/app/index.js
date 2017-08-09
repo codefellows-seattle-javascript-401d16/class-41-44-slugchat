de98a966c8a0e658ebf4b50f88f48d9a071f7b79
Amanda Koster <amandakoster@gmail.com>
2017-08-08 18:01:33 -0700

    componentes are built

---
+++ frontend/component/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {MemoryRouter, Switch, Route} from 'react-router-dom'
+import * as util from '../../lib/util.js'
+import * as auth from '../../action/auth.js'
+import * as route from '../../action/route.js'
+
+import 

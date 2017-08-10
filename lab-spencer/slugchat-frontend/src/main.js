af89c96c60b8160b702af90669b2650f59556262
Spencer Gietzen <Speng1516@comcast.net>
2017-08-09 18:10:46 -0700

    front end io and subscriber setup

--- lab-spencer/slugchat-frontend/src/main.js
+++ lab-spencer/slugchat-frontend/src/main.js
@@ -4,6 +4,15 @@ import ReactDom from 'react-dom';
 import App from './component/app';
 import {Provider} from 'react-redux';
 import appStoreCreate from './lib/app-store-create.js';
+import io from './lib/io.js';
+
+import userSubscribers from './subscribe/user.js';
+import messageSubscribers from './subscribe/message.js';
+
+const store = appStoreCreate();
+
+let subscribers = Object.assign(userSubscribers, messageSubscribers);
+io(store, subscribers);
 
 let AppContainer = () => (
   <Provider store={appStoreCreate()}>
